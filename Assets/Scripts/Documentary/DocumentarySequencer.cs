34d4065f3325a34803231a9e38bfc0b30c4a5307
K_C_DIGNAN <74738806+K-C-DIGNAN@users.noreply.github.com>
2022-11-28 17:54:02 +0000

    Plunder07 + Plunder08

--- Assets/Scripts/Documentary/DocumentarySequencer.cs
+++ Assets/Scripts/Documentary/DocumentarySequencer.cs
@@ -8,6 +8,8 @@ public class DocumentarySequencer : MonoBehaviour {
     private Stack<string> scenes = new Stack<string>(
         new string[] {
             "Mutiny01a",
+            "Plunder08",
+            "Plunder07",
             "Plunder06",
             "Plunder05",
             "Plunder04", 
